84cecd164ee8e862971a960c1b395ee6f622a8df
medsignviren <medsignviren@gmail.com>
2024-09-24 23:54:43 +0530

    udated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NotePad from "./NotePad";
+import { Link } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    // Detect if the page was reloaded
+    if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+      // Redirect to another domain
+      window.location.href = "https://livenotepad-five.vercel.app";
+    }
+  }, []); // Empty dependency array ensures it runs once on page load
+
   return (
     <>
-      <NotePad />
+    <div className="grid justify-center items-center gap-4">
+      
+    <h1 className="text-center">Hello!!</h1>
+    <Link to="https://livenotepad-five.vercel.app/" className="">https://livenotepad-five.vercel.app</Link>
+      {/* <NotePad /> */}
+    </div>
     </>
   );
 }
